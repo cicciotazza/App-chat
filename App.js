f7a273dc1042be09f80863e695e08a167365716e
cicciotazza <cicciotazza@hotmail.com>
2022-02-12 13:05:56 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StyleSheet, View, Text, TextInput, Button } from ReactNative;
+import react, { component } from 'react';
+import Screen1 from './components/Start';
+import Screen2 from './components/Chat';
+import 'react-native-gesture-handler';
+//import React Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
+  //
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Screen1">
+          <Stack.Screen name="Screen1" component={Screen1} />
+          <Stack.Screen name="Screen2" component={Screen2} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Start.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default class Screen1 extends React.Component {
+  render() {
+    let name = this.props.route.params.name; 
+    // or ...   let { name } = this.props.route.params;
+    this.props.navigation.setOptions({ title: name });
+    
+    return (
+      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Hello Screen1!</Text>
+        <Button
+          title="Go to Screen 2"
+          onPress={() => this.props.navigation.navigate('Screen2')}
+        />
+      </View>
+    )
+  }
+}
